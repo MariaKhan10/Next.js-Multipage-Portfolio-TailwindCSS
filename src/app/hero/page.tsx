586dfcd6d3da7dbbf292ac755da1500b2fd4fb15
Maria <mariapervaiz40@gmail.com>
2024-11-11 20:50:48 +0500

    contact form validation

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
 
   return (
     <section className="text-white body-font">
-      <div style={{ background: gradientBackground }} className="py-29">
+      <div style={{ background: gradientBackground }} className="py-34">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
